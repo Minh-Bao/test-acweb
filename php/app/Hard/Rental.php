5d59fac39f7e8f40b886410850a75fdac1e8c2b8
minh-bao <mbnguyen@hotmail.fr>
2023-05-13 04:44:48 +0200

    docs(Hard): suppression de commentaire dupliqué

--- php/app/Hard/Rental.php
+++ php/app/Hard/Rental.php
@@ -1,6 +1,5 @@
 <?php
 /**
- * J'ai redeclaré les propriétés et mis daysRented en Readonly afin d'éviter une modification externe
  * J'ai supprimé la méthode getDaysRented et getMovie comme le permet php8 pour les propriété public
  * J'ai passé la propriété daysRented en readonly afin d'éviter toute modif externe à  la classe
  *
